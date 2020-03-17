1b4dade74c20fb77fc772cde78e797931baa99ba
Nicolai-cmd <60780076+Nicolai-cmd@users.noreply.github.com>
2020-03-17 10:32:22 +0200

    Add files via upload

---
+++ MyAccount.php
@@ -0,0 +1,70 @@
+<?php include 'header.php';?>
+<div class="container">
+<div class="row">
+  <div class="col-sm-9">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Subscription</h5>
+        <p class="card-text">Subscribe to support us.</p>
+        <a href="#" class="button button1">Subscribe</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">€0</h5>
+        <p class="card-text">On the WTW account</p>
+        <a href="#" class="button button1">Add funds</a>
+      </div>
+    </div>
+  </div>
+</div>
+<hr>
+<div class="row">
+  <div class="col-sm-12">
+  <center><a href="#" class="button button1">
+          <span class="fa fa-bell"></span> Notification
+        </a>
+</center>
+          </div>
+        </div>
+    <hr>
+  </div>
+</div>
+
+<div class="container">
+<center>
+<div class="row">
+  <div class="col-sm-3">
+     <p>
+        <a href="deleteupdate.php" class="button button1">
+          <span class="glyphicon glyphicon-shopping-cart"></span> Delete/Update
+        </a>
+      </p> 
+    </div>
+    <div class="col-sm-3">
+     <p>
+     <a href="read.php" class="button button1">
+          <span class="glyphicon glyphicon-time"></span> Read info
+        </a>
+      </p> 
+    </div>
+    <div class="col-sm-3">
+     <p>
+        <a href="#" class="button button1">
+          <span class="glyphicon glyphicon-shopping-cart"></span> Support help
+        </a>
+      </p> 
+    </div>
+    <div class="col-sm-3">
+     <p>
+     <a href="#" class="button button1">
+          <span class="glyphicon glyphicon-cog"></span> Settings
+        </a>
+      </p> 
+    </div>
+  </div>
+  </center>
+</div>
+<?php include 'footer.php';?>
